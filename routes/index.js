cb739b580dbef1cdfc62078ee892e42f22f52fda
Jason Rey <jasonrey@outlook.com>
2014-12-31 13:17:05 +0800

    Updated login view to handle error message

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,16 @@ router.get('/', function(req, res) {
 
 // Get login page
 router.get('/login', function(req, res) {
-  res.render('login');
+  var message = null;
+
+  // If there is a login error, set the message then clear it because we only want this once
+  if (req.session.loginError !== undefined) {
+    message = req.session.loginError;
+    req.session.loginError = undefined;
+  }
+  res.render('login', {
+    message: message
+  });
 });
 
 // Get register page
