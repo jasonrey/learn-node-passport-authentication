66455c005b40231d68919ac845328af9184d5ee3
Jason Rey <jasonrey@outlook.com>
2015-01-01 14:32:21 +0800

    Updated get routes to handle error message from session

--- routes/index.js
+++ routes/index.js
@@ -8,19 +8,35 @@ router.get('/', function(req, res) {
 
 // Get login page
 router.get('/login', function(req, res) {
-  res.render('login');
+  var message = req.session.message;
+  req.session.message = null;
+
+  res.render('login', {
+    message: message
+  });
 });
 
 // Get register page
 router.get('/register', function(req, res) {
-  res.render('register');
+  var message = req.session.message;
+  req.session.message = null;
+
+  res.render('register', {
+    message: message
+  });
 });
 
 // Get profile page
 router.get('/profile', function(req, res) {
+  // If no user is in the session, then we redirect out to login page
+  if (req.user === undefined || !req.user) {
+    req.session.message = 'Not logged in.';
+    return res.redirect('/login');
+  }
+
   res.render('profile', {
     user: req.user
   });
-})
+});
 
 module.exports = router;
